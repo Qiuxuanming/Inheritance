28c6f506fa4844b9230c1700a60872017cda4921
Qiuxuanming <2371620359@qq.com>
2025-03-21 16:13:24 +0800

    exercise 10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        StoreOneA store = new StoreOneA(true, 100000, 12, true);
+        store.enterStoreDetails();
+        store.showStoreInfo();
+        double monthlyPayment = store.calculateLoanFinancing();
+        System.out.println("Monthly Loan Payment: " + monthlyPayment);
     }
-}
+}    
--- src/StoreToRent.java
+++ src/StoreToRent.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 
 public class StoreToRent {
-    
+
     private static final double MAINTENANCE_COST = 1000;
-    
-    
+    private static final double INTEREST_RATE = 0.25;
+    private boolean loanRequired;
+    private double loanAmount;
+    private int loanPaymentTerm;
+
     private String storeName;
     private String storeBusiness;
     private double totalArea;
@@ -14,6 +17,12 @@ public class StoreToRent {
     private String minimumLeasePeriod;
     private String floorNumber;
     private boolean available;
+
+    public StoreToRent(boolean loanRequired, double loanAmount, int loanPaymentTerm) {
+        this.loanRequired = loanRequired;
+        this.loanAmount = loanAmount;
+        this.loanPaymentTerm = loanPaymentTerm;
+    }
 
     public String getStoreName() {
         return storeName;
@@ -79,7 +88,7 @@ public class StoreToRent {
         this.available = available;
     }
 
-    public void enterStoreDetails(){
+    public void enterStoreDetails() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Store Name: ");
         setStoreName(input.nextLine());
@@ -99,22 +108,49 @@ public class StoreToRent {
         System.out.println("Enter Floor Number: ");
         setFloorNumber(input.nextLine());
         System.out.println("Enter Available: ");
-        setAvailable(input.nextBoolean());    
+        setAvailable(input.nextBoolean());
         input.close();
-        
+
+    }
+
+    public double getINTEREST_RATE() {
+        return INTEREST_RATE;
+    }
+
+    public boolean isLoanRequired() {
+        return loanRequired;
+    }
+
+    public double getLoanAmount() {
+        return loanAmount;
+    }
+
+    public int getLoanPaymentTerm() {
+        return loanPaymentTerm;
+    }
+
+    public double calculateLoanFinancing() {
+        if (loanRequired) {
+            return (loanAmount * (1 + INTEREST_RATE)) / loanPaymentTerm;
+        }
+        return 0;
     }
 
     @Override
     public String toString() {
-        return "\n" + 
-        "Store Name: " + storeName + "\n" +
-        "Store Business: " + storeBusiness + "\n" +
-        "Total Area (sq.m): " + totalArea + "\n" + 
-        "Selling Price: €" + sellingPrice + "\n" +
-        "Rent: €" + rent + "\n" +
-        "Minimum Lease Period: " + minimumLeasePeriod + "\n" +
-        "Floor Number: " + floorNumber + "\n" +
-        "Maintenance Cost: €" + MAINTENANCE_COST + "\n" +
-        "Available: " + available + "\n";
+        return "\n" +
+                "Store Name: " + storeName + "\n" +
+                "Store Business: " + storeBusiness + "\n" +
+                "Total Area (sq.m): " + totalArea + "\n" +
+                "Selling Price: €" + sellingPrice + "\n" +
+                "Rent: €" + rent + "\n" +
+                "Minimum Lease Period: " + minimumLeasePeriod + "\n" +
+                "Floor Number: " + floorNumber + "\n" +
+                "Maintenance Cost: €" + MAINTENANCE_COST + "\n" +
+                "Available: " + available + "\n" +
+                "Loan Required: " + loanRequired + "\n" +
+                "Loan Amount: " + loanAmount + "\n" +
+                "Loan Payment Term: " + loanPaymentTerm + "\n" +
+                "Interest Rate: " + INTEREST_RATE + "\n";
     }
-}
+}    
